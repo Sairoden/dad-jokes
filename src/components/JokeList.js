36bc2cee1578093afa48c57374e9e9b83a2c4711
Sairoden <odingandarosa@gmail.com>
2021-12-08 16:49:17 +0800

    Fix local storage bug

--- src/components/JokeList.js
+++ src/components/JokeList.js
@@ -14,6 +14,7 @@ class JokeList extends Component {
     this.state = {
       jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -31,7 +32,13 @@ class JokeList extends Component {
         jokes.push({ id: uuid(), text: res.data.joke, votes: 0 });
       }
 
-      this.setState({ jokes: jokes });
+      this.setState(
+        state => ({
+          okes: [...state.jokes, ...jokes],
+        }),
+        () =>
+          window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+      );
       window.localStorage.setItem("jokes", JSON.stringify(jokes));
     } catch (err) {
       console.error("Oops something went wrong! 😣😣", err.message);
@@ -39,11 +46,19 @@ class JokeList extends Component {
   }
 
   handleVote(id, delta) {
-    this.setState(state => ({
-      jokes: state.jokes.map(joke =>
-        joke.id === id ? { ...joke, votes: joke.votes + delta } : joke
-      ),
-    }));
+    this.setState(
+      state => ({
+        jokes: state.jokes.map(joke =>
+          joke.id === id ? { ...joke, votes: joke.votes + delta } : joke
+        ),
+      }),
+      () =>
+        window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
+    );
+  }
+
+  handleClick(id, delta) {
+    this.getJokes();
   }
 
   render() {
@@ -57,7 +72,9 @@ class JokeList extends Component {
             src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
             alt="Dad Joke"
           />
-          <button className="JokeList-getmore">New Jokes</button>
+          <button onClick={this.handleClick} className="JokeList-getmore">
+            New Jokes
+          </button>
         </div>
 
         <div className="JokeList-jokes">
